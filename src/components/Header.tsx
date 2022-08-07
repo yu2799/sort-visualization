40cfc53a7ff676d6be6aaf0084bee5167f76f147
yu2799 <ura.yu.2799@gmail.com>
2022-08-07 21:47:32 +0900

    add site info

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,45 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import { useState } from "react";
 
 export const Header = (): JSX.Element => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component="div">
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Sort Visualization
         </Typography>
+        <IconButton
+          size="large"
+          color="inherit"
+          aria-label="menu"
+          onClick={() => setOpen(true)}
+        >
+          <HelpOutlineIcon />
+        </IconButton>
       </Toolbar>
+      <Dialog onClose={() => setOpen(false)} open={open}>
+        <DialogTitle>サイトについて</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            ソート中の様子を見ることができます。
+          </DialogContentText>
+          <DialogContentText>
+            見かけの走査時間と実際の走査時間は異なります。
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
     </AppBar>
   );
 };
