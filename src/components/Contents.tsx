9072e66379dfbdb2532bdcb0a3f66d0718cf6bf8
yu2799 <ura.yu.2799@gmail.com>
2022-08-07 15:29:44 +0900

    add selection sort

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -2,7 +2,7 @@ import * as d3 from "d3";
 import { Button, Container } from "@mui/material";
 import { useState, useCallback, Dispatch, SetStateAction } from "react";
 
-const DATA_LENGTH = 50;
+const DATA_LENGTH = 100;
 
 export const Contents = (): JSX.Element => {
   const [data, setData] = useState<number[]>(
@@ -10,6 +10,7 @@ export const Contents = (): JSX.Element => {
   );
   const [loading, setLoading] = useState<boolean>(false);
   const [shuffled, setShuffled] = useState<boolean>(false);
+  const [compareCnt, setCompareCnt] = useState<number>(0);
   const [cnt, setCnt] = useState<number>(0);
 
   const height: number = 200;
@@ -33,6 +34,8 @@ export const Contents = (): JSX.Element => {
 
   const handleShuffled = () => {
     setLoading(true);
+    setCompareCnt(0);
+    setCnt(0);
     const shuffle = async (
       [...array]: number[],
       iterCnt: number
@@ -55,15 +58,16 @@ export const Contents = (): JSX.Element => {
   return (
     <Container sx={{ py: 3 }}>
       <svg viewBox={`0 0 ${width} ${height}`}>
-        <text y="10" fontSize="10">
-          {`交換回数：${cnt} 回`}
-        </text>
+        <g fontSize="5">
+          <text y="5">{`比較回数：${compareCnt} 回`}</text>
+          <text y="15">{`交換回数：${cnt} 回`}</text>
+        </g>
         <g transform={`translate(${width},${height}) rotate(180)`}>
           {data.map((num, idx) => {
             return (
               <rect
                 key={idx}
-                x={(width * (DATA_LENGTH - idx)) / DATA_LENGTH}
+                x={(width * (DATA_LENGTH - idx - 1)) / DATA_LENGTH}
                 y={0}
                 width={width / DATA_LENGTH}
                 height={yScale(num) * 0.9}
@@ -86,17 +90,28 @@ export const Contents = (): JSX.Element => {
 
       <Button
         variant="contained"
-        onClick={() => sort(bubbleSort(data, setCnt, setData))}
+        onClick={() => sort(bubbleSort(data, setCompareCnt, setCnt, setData))}
         disabled={loading || !shuffled}
       >
         bubble
       </Button>
       <Button
         variant="contained"
-        onClick={() => sort(insertionSort(data, setCnt, setData))}
+        onClick={() =>
+          sort(insertionSort(data, setCompareCnt, setCnt, setData))
+        }
         disabled={loading || !shuffled}
       >
         insertion
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() =>
+          sort(selectionSort(data, setCompareCnt, setCnt, setData))
+        }
+        disabled={loading || !shuffled}
+      >
+        selection
       </Button>
     </Container>
   );
@@ -104,34 +119,61 @@ export const Contents = (): JSX.Element => {
 
 const bubbleSort = async (
   [...array]: number[],
+  setCompareCnt: Dispatch<SetStateAction<number>>,
   setCnt: Dispatch<SetStateAction<number>>,
   setData: Dispatch<SetStateAction<number[]>>
 ) => {
-  for (let i = 0; i < DATA_LENGTH - 1; ++i) {
-    for (let j = 1; j < DATA_LENGTH - i; ++j) {
+  for (let i = 0; i < array.length - 1; ++i) {
+    for (let j = 1; j < array.length - i; ++j) {
       if (array[j] < array[j - 1]) {
         [array[j - 1], array[j]] = [array[j], array[j - 1]];
         setCnt((prev) => prev + 1);
       }
-      await new Promise((resolve) => setTimeout(resolve, 1 / 2 ** 10));
+      setCompareCnt((prev) => prev + 1);
       setData(array.slice());
+      await new Promise((resolve) => setTimeout(resolve, 1 / 2 ** 10));
     }
   }
 };
 
 const insertionSort = async (
   [...array]: number[],
+  setCompareCnt: Dispatch<SetStateAction<number>>,
   setCnt: Dispatch<SetStateAction<number>>,
   setData: Dispatch<SetStateAction<number[]>>
 ) => {
-  for (let i = 1; i < DATA_LENGTH; ++i) {
+  for (let i = 1; i < array.length; ++i) {
     let j = i;
     while (j !== 0 && array[j - 1] > array[j]) {
       [array[j - 1], array[j]] = [array[j], array[j - 1]];
       --j;
       setCnt((prev) => prev + 1);
+      setCompareCnt((prev) => prev + 1);
       setData(array.slice());
       await new Promise((resolve) => setTimeout(resolve, 1 / 2 ** 10));
     }
+  }
+};
+
+const selectionSort = async (
+  [...array]: number[],
+  setCompareCnt: Dispatch<SetStateAction<number>>,
+  setCnt: Dispatch<SetStateAction<number>>,
+  setData: Dispatch<SetStateAction<number[]>>
+) => {
+  for (let i = 0; i < array.length - 1; ++i) {
+    let idx = i;
+    let tmpMin = array[i];
+    for (let j = i + 1; j < array.length; ++j) {
+      setCompareCnt((prev) => prev + 1);
+      if (array[j] < tmpMin) {
+        tmpMin = array[j];
+        idx = j;
+      }
+    }
+    [array[idx], array[i]] = [array[i], array[idx]];
+    setCnt((prev) => prev + 1);
+    setData(array.slice());
+    await new Promise((resolve) => setTimeout(resolve, 1 / 2 ** 10));
   }
 };
